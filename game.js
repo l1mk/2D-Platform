2c9644b620025f07622be8e315be90d8706138e2
l1mk <jhonxlink@hotmail.com>
2021-08-08 15:30:40 -0500

    finished building second game and lose mechanics

--- game.js
+++ game.js
@@ -7,9 +7,13 @@ kaboom ({
 })
 
 const MOVE_SPEED = 120;
+const BOOTS_MOVE_SPEED = 240;
 const JUMP_FORCE = 380;
 const BIG_JUMP_FORCE = 450;
-const ENEMY_SPEED = 20;
+const ENEMY_SPEED = 40;
+const FALL_DEATH = 800;
+let isJumping = true;
+let CURRENT_SPEED = MOVE_SPEED;
 let CURRENT_JUMP_FORCE = JUMP_FORCE;
 let score = 0;
 
@@ -24,53 +28,92 @@ loadSprite('unmetal', 'e7ySdHi.png');
 loadSprite('metal', 'xkFtAgH.png');
 loadSprite('boots', 'SmweKjq.png');
 loadSprite('chest', '97WPe55.png');
+loadSprite('pokecenter', 'nY0THDK.png');
+loadSprite('grass_bg', 'AFqJ3Rd.png');
 
-scene("game", ({score})=> {
+scene("game", ({level, score})=> {
     layers(['bg', 'obj', 'ui'], 'obj')
 
-    const map = [
-        '                                                        ',
-        '                                                        ',
-        '                                                        ',
-        '                                                        ',
-        '                                                        ',
-        '                                                        ',
-        '                    -?-                                 ',
-        '                                                        ',
-        '                                                        ',
-        '                  -xx      xx      x                    ',
-        '                                                        ',
-        '                                                        ',
-        '           x------x                  xxx----------      ',
-        '                                                        ',
-        '                          0                          ^  ',
-        '===================  ==========    =====================',
+    const maps = [
+        [
+            '                                                                      ',
+            '                                                                      ',
+            '                                                                      ',
+            '                                                                      ',
+            '                                                                      ',
+            '                                                                      ',
+            '                    -?-               xxx--                           ',
+            '                                                                      ',
+            '                                                                      ',
+            '                  -xx      xx      x                                  ',
+            '      --x                                               P             ',
+            '                                                                      ',
+            '           x------x                  xxx----------                    ',
+            '                            0                        ^              ^ ',
+            '...................     .......    ...................................',
+            '===================     =======    ===================================',
+        ],
+        [
+        '                                                                                  ',
+        '                                                                                  ',
+        '                                                                                  ',
+        '                                            ^   0                                 ',
+        '             x?xx                   xxxxxxxxxxxxxxxxxx?xxxxx                      ',            
+        '                                                                                  ',
+        '                         ^                                                        ',
+        '            xx xxx        xxx            xxxxx             xxxx         ^         ',
+        '                                                                 xxxx             ',
+        '                   x                                   x  x                        ',
+        '-                        ^                                                        -',
+        '-                  xxxxxxxxx      xx      xxxxx x      x        ^xx               -',
+        '-                                                                                 -',
+        '-                                      xx                           xxxx          -',
+        '-       xxxx                                                            xxxx     -',
+        '-                                 xxxx                xxxxx                      --',
+        '-           ^      ^        ^   ^         ^          ^     ^                    ---',
+        '----------------------------------------------------------------------------------',
+        '----------------------------------------------------------------------------------',]
+
     ];
 
     const levelCfg = {
         width: 25,
         height: 25,
+        '.': [sprite('grass_bg'), scale(0.1), layer('bg')],
         '=': [sprite('grass'), solid(), scale(0.1)],
-        '0': [sprite('pokeball'), solid(), scale(0.1)],
+        '0': [sprite('pokeball'), solid(), body(), scale(0.1)],
         '-': [sprite('bricks'), solid(), 'coin-surprise', scale(0.1)],
         '^': [sprite('pikachu'), solid(), 'dangerous', body(), scale(0.1)],
         'x': [sprite('metal'), solid(), 'undestructable', scale(0.1)],
-        '$': [sprite('coin'), solid(), 'coin', body(), scale(0.1)],
+        '$': [sprite('coin'), 'coin', body(), scale(0.1)],
         '{': [sprite('unmetal'), solid(), scale(0.1)],
-        'b': [sprite('boots'), solid(), 'boots', scale(0.1)],
+        'b': [sprite('boots'), 'boots',  body(), scale(0.1)],
         '?': [sprite('chest'), solid(), 'boots-surprise', scale(0.1)],
+        'P': [sprite('pokecenter'), 'door', scale(0.5)],
+
     };
 
-    const gameLevel = addLevel(map, levelCfg);
+    const gameLevel = addLevel(maps[level], levelCfg);
+
+    const scoreText = add([
+        text("SCORE"),
+        pos(20,60),
+        layer('ui'),
+        {
+            value: "SCORE",
+        }
+    ]);
 
     const scoreLabel = add([
         text(score),
-        pos(30,60),
+        pos(80,60),
         layer('ui'),
         {
             value: score,
         }
     ]);
+
+    add([text('level ' + parseInt(level + 1)), pos(800 ,60)])
 
     function bigJump(){
         let timer = 0;
@@ -89,6 +132,7 @@ scene("game", ({score})=> {
             },
             smallify(){
                 this.scale = vec2(0.1)
+                CURRENT_SPEED = MOVE_SPEED
                 CURRENT_JUMP_FORCE = JUMP_FORCE
                 timer = 0
                 isBig = false
@@ -96,6 +140,7 @@ scene("game", ({score})=> {
             biggify(time) {
                 this.scale = vec2(0.18)
                 CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+                CURRENT_SPEED = BOOTS_MOVE_SPEED
                 timer = time
                 isBig = true
             }
@@ -143,20 +188,47 @@ scene("game", ({score})=> {
     })
 
     player.collides('dangerous', (d) => {
-        go('lose', { score: scoreLabel.value})
+        if (isJumping) {
+            destroy(d)
+        } else {
+            go('lose', { score: scoreLabel.value})
+        }
+    })
+
+    player.collides('door', () => {
+        keyPress('up', () => {
+            go('game', {
+                level: (level + 1),
+                score: scoreLabel.value
+            })
+        })
+    })
+
+    player.action(() => {
+        camPos(player.pos)
+        if (player.pos.y >= FALL_DEATH) {
+            go('lose', { score: scoreLabel.value})
+        }
     })
 
     keyDown('left', () => {
-        player.move(-MOVE_SPEED, 0)
+        player.move(-CURRENT_SPEED, 0)
     });
 
     keyDown('right', () => {
-        player.move(MOVE_SPEED, 0)
+        player.move(CURRENT_SPEED, 0)
     });
+
+    player.action(() => {
+        if (player.grounded()) {
+            isJumping = false;
+        }
+    })
 
     keyPress('space', () => {
         if (player.grounded()){
-            player.jump(CURRENT_JUMP_FORCE)
+            isJumping = true;
+            player.jump(CURRENT_JUMP_FORCE);
         }
     });
 
@@ -165,7 +237,8 @@ scene("game", ({score})=> {
 });
 
 scene('lose', ({score}) => {
-    add([text(score, 32), origin('center', pos(width()/2, height()/2))])
+    add([text("Your Score is", 25), origin('center'), pos(width() / 2, height() / 2.5)])
+    add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)])
 })
 
-start('game', {score: 0});
+start('game', {level: 0, score: 0});
